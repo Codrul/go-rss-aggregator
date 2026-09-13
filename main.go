6402a3daf847a2379535757a63dd4651fe93c35c
Codrul <codreanu.andrei1125@gmail.com>
2026-09-13 13:04:12 +0300

    add cors vendor

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -19,6 +20,14 @@ func main(){
 	}
 
 	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: false,
+		MaxAge: 				300,			
+	}))
 
 	server := &http.Server{
 		Handler: router,
